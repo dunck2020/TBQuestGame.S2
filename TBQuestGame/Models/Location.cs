d2441564c47c9b2b45597dcc6110130673ec612d
John Dunckel <j.dunckel@hotmail.com>
2021-03-07 21:28:49 -0500

    Add project files.

---
+++ TBQuestGame/Models/Location.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBQuestGame.Models
+{
+    public class Location
+    {
+        #region FIELDS
+
+        private int _id;
+        private string _name;
+        private string _description;
+        private bool _isAccessible;
+        private string _locationMessage;
+        private int _modifyLives;
+        private int _modifyHealth;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public bool IsAccessible
+        {
+            get { return _isAccessible; }
+            set { _isAccessible = value; }
+        }
+        public string LocationMessage
+        {
+            get { return _locationMessage; }
+            set { _locationMessage = value; }
+        }
+        public int ModifyLives
+        {
+            get { return _modifyLives; }
+            set { _modifyLives = value; }
+        }
+        public int ModifyHealth
+        {
+            get { return _modifyHealth; }
+            set { _modifyHealth = value; }
+        }
+
+        #endregion
+    }
+}
